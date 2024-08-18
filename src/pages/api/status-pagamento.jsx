03aa1f44108601daa83f179f41118bb8c03b5d00
leoleoyuuki <rm550373@fiap.com.br>
2024-08-18 19:40:09 -0300

    feat: Update MercadoPago integration API endpoints and add Express server

--- src/pages/api/status-pagamento.jsx
+++ src/pages/api/status-pagamento.jsx
@@ -1,40 +1,51 @@
-import mercadopago from "mercadopago";
+// import mercadopago from "mercadopago";
+
+// export default async function handler(req, res) {
+//   if (req.method === "POST") {
+//     const topic = req.body.topic;
+//     const data = req.body.data;
+
+//     if (topic === "payment.updated") {
+//       const paymentId = data.id;
+
+//       try {
+//         // Verifica o status do pagamento
+//         const payment = await mercadopago.payment.get(paymentId);
+
+//         if (payment.body.status === "approved") {
+//           // Pagamento aprovado, execute as ações desejadas
+//           console.log("Pagamento aprovado:", payment.body);
+
+//           // Exemplo: Enviar e-mail de confirmação
+//           sendEmail(payment.body.payer.email, "Pagamento confirmado");
+
+//           // Exemplo: Atualizar o status do pedido no banco de dados
+//           updateOrderStatus(paymentId, "pago");
+//         } else {
+//           console.log("Pagamento não aprovado:", payment.body);
+//         }
+//       } catch (error) {
+//         console.error("Erro ao processar a notificação:", error);
+//       }
+//     }
+
+//     res.sendStatus(200);
+//   } else {
+//     res.setHeader("Allow", "POST");
+//     res.status(405).end("Method Not Allowed");
+//   }
+// }
+
+
 
 export default async function handler(req, res) {
-  if (req.method === "POST") {
-    const topic = req.body.topic;
-    const data = req.body.data;
+    if(req.method === "POST"){
+        const hello = req.hello;
 
-    if (topic === "payment.updated") {
-      const paymentId = data.id;
-
-      try {
-        // Verifica o status do pagamento
-        const payment = await mercadopago.payment.get(paymentId);
-
-        if (payment.body.status === "approved") {
-          // Pagamento aprovado, execute as ações desejadas
-          console.log("Pagamento aprovado:", payment.body);
-
-          localStorage.setItem('paymentStatus', 'success');
-          // Exemplo: Enviar e-mail de confirmação
-        //   sendEmail(payment.body.payer.email, "Pagamento confirmado");
-
-          // Exemplo: Atualizar o status do pedido no banco de dados
-        //   updateOrderStatus(paymentId, "pago");
-        } else {
-          console.log("Pagamento não aprovado:", payment.body);
-        }
-      } catch (error) {
-        console.error("Erro ao processar a notificação:", error);
-      }
+        res.status(200).json({status: "REQUISIÇAO RECEBIDA"});	
     }
-
-    res.sendStatus(200);
-  } else {
-    res.setHeader("Allow", "POST");
-    res.status(405).end("Method Not Allowed");
+    else if (req.method === "GET"){
+        res.status(200).json({status: "GET RECEBIDO"});
+    }
   }
-}
-
-
+  
