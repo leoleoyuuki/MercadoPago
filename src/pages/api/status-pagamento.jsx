0ca7576de044f4c9acad877b5be535a37e36eaa6
leoleoyuuki <rm550373@fiap.com.br>
2024-08-18 18:28:05 -0300

    feat: Update MercadoPago integration API endpoints and add Express server
    This commit updates the MercadoPago integration API endpoints and adds an Express server to the project. The Express server is configured to listen on port 3000 and handle POST requests to the '/status-pagamento' route. The code for handling payment notifications has been uncommented and updated to use the Express framework. Additionally, the necessary dependencies for Express have been added to the package.json file.

--- src/pages/api/status-pagamento.jsx
+++ src/pages/api/status-pagamento.jsx
@@ -38,49 +38,11 @@
 
 
 
-var express = require('express');
-import mercadopago from 'mercadopago';
+export default async function handler(req, res) {
+    if(req.method === "POST"){
+        const hello = req.hello;
 
-const app = express();
-const port = 3000;
-
-// Configura as credenciais do Mercado Pago
-mercadopago.configure({
-  access_token: 'APP_USR-5451710554063836-081815-7828d7b3758fec037016cf253d6dcc62-486596499'
-});
-
-// Rota para receber as notificações
-app.post('/status-pagamento', async (req, res) => {
-  const topic = req.body.topic;
-  const data = req.body.data;
-
-  if (topic === 'payment.updated') {
-    const paymentId = data.id;
-
-    try {
-      // Verifica o status do pagamento
-      const payment = await mercadopago.payment.get(paymentId);
-
-      if (payment.body.status === 'approved') {
-        // Pagamento aprovado, execute as ações desejadas
-        console.log('Pagamento aprovado:', payment.body);
-
-        // Exemplo: Enviar e-mail de confirmação
-        sendEmail(payment.body.payer.email, 'Pagamento confirmado');
-
-        // Exemplo: Atualizar o status do pedido no banco de dados
-        updateOrderStatus(paymentId, 'pago');
-      } else {
-        console.log('Pagamento não aprovado:', payment.body);
-      }
-    } catch (error) {
-      console.error('Erro ao processar a notificação:', error);
+        res.status(200).json({status: "REQUISIÇAO RECEBIDA"});	
     }
   }
-
-  res.sendStatus(200);
-});
-
-app.listen(port, () => {
-  console.log(`Servidor ouvindo na porta ${port}`);
-});
+  
