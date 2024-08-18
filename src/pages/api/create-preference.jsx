2847cd6645d35f564577cc3a81837fc5ca8b9402
leoleoyuuki <rm550373@fiap.com.br>
2024-08-18 18:01:40 -0300

    test

--- src/pages/api/create-preference.jsx
+++ src/pages/api/create-preference.jsx
@@ -50,6 +50,6 @@ export default async function handler(req, res) {
     }
   } else {
     res.setHeader('Allow', 'POST');
-    res.status(405).end('Method Not Allowed');
+    res.status(405).end('Method Not Allowed dd');
   }
 }
