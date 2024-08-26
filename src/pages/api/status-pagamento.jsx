40fef10602454ee41f07b2ec3ce812760f6d730b
leoleoyuuki <rm550373@fiap.com.br>
2024-08-26 13:53:50 -0300

    teste

--- src/pages/api/status-pagamento.jsx
+++ src/pages/api/status-pagamento.jsx
@@ -1,5 +1,4 @@
 
-import mercadopago from "mercadopago";
 import {addDoc,collection,db} from "../../service/firebasesdk"
 
 
@@ -18,37 +17,9 @@ const addItem = async () => {
 export default async function handler(req, res) {
   
   if (req.method === "POST") {
-    const hello = req.hello;
-    const topic = req.body.topic;
-    const data = req.body.data;
     console.log("requisição recebida", req.body);
     addItem()
-    if (topic === "payment.updated") {
-      const paymentId = data.id;
-
-      try {
-        // Verifica o status do pagamento
-        const payment = await mercadopago.payment.get(paymentId);
-
-        if (payment.body.status === "approved") {
-          // Pagamento aprovado, execute as ações desejadas
-          console.log("Pagamento aprovado:", payment.body);
-          addItem()
-
-          // Exemplo: Enviar e-mail de confirmação
-        //   sendEmail(payment.body.payer.email, "Pagamento confirmado");
-
-          // Exemplo: Atualizar o status do pedido no banco de dados
-        //   updateOrderStatus(paymentId, "pago");
-        } else {
-          addItem()
-          console.log("Pagamento não aprovado:", payment.body);
-        }
-      } catch (error) {
-        addItem()
-        console.error("Erro ao processar a notificação:", error);
-      }
-    }
+    
     res.status(200).json({ status: "REQUISIÇAO RECEBIDA" });
   } else if (req.method === "GET") {
     res.status(200).json({ status: "GET RECEBIDO" });
