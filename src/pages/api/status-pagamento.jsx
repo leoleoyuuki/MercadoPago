7d6312bf85496b2b26a377a3f1e3fa3279238ba7
leoleoyuuki <rm550373@fiap.com.br>
2024-08-26 12:24:57 -0300

    teste

--- src/pages/api/status-pagamento.jsx
+++ src/pages/api/status-pagamento.jsx
@@ -1,24 +1,45 @@
+// import mercadopago from "mercadopago";
+
+// export default async function handler(req, res) {
+//   if (req.method === "POST") {
+//     const topic = req.body.topic;
+//     const data = req.body.data;
+
+//     if (topic === "payment.updated") {
+//       const paymentId = data.id;
+
+//       try {
+//         // Verifica o status do pagamento
+//         const payment = await mercadopago.payment.get(paymentId);
+
+//         if (payment.body.status === "approved") {
+//           // Pagamento aprovado, execute as ações desejadas
+//           console.log("Pagamento aprovado:", payment.body);
+
+//           // Exemplo: Enviar e-mail de confirmação
+//           sendEmail(payment.body.payer.email, "Pagamento confirmado");
+
+//           // Exemplo: Atualizar o status do pedido no banco de dados
+//           updateOrderStatus(paymentId, "pago");
+//         } else {
+//           console.log("Pagamento não aprovado:", payment.body);
+//         }
+//       } catch (error) {
+//         console.error("Erro ao processar a notificação:", error);
+//       }
+//     }
+
+//     res.sendStatus(200);
+//   } else {
+//     res.setHeader("Allow", "POST");
+//     res.status(405).end("Method Not Allowed");
+//   }
+// }
+
 
 import mercadopago from "mercadopago";
-import {addDoc,collection,db,deleteDoc,doc,getDocs,updateDoc} from "../../service/firebasesdk"
-
-
-const addItem = async () => {
-  try {
-    const docRef = await addDoc(collection(db, "Pessoas"), {
-      Nome: "FUNCIONOU"
-    });
-    console.log("Document written with ID: ", docRef.id);
-    alert("Produto Cadastrado");
-    getItem()
-  } catch (e) {
-    console.error("Error adding document: ", e);
-  }
-};
-
 
 export default async function handler(req, res) {
-  
   if (req.method === "POST") {
     const hello = req.hello;
     const topic = req.body.topic;
@@ -34,7 +55,6 @@ export default async function handler(req, res) {
         if (payment.body.status === "approved") {
           // Pagamento aprovado, execute as ações desejadas
           console.log("Pagamento aprovado:", payment.body);
-          addItem()
 
           // Exemplo: Enviar e-mail de confirmação
         //   sendEmail(payment.body.payer.email, "Pagamento confirmado");
@@ -42,18 +62,15 @@ export default async function handler(req, res) {
           // Exemplo: Atualizar o status do pedido no banco de dados
         //   updateOrderStatus(paymentId, "pago");
         } else {
-          addItem()
           console.log("Pagamento não aprovado:", payment.body);
         }
       } catch (error) {
-        addItem()
         console.error("Erro ao processar a notificação:", error);
       }
     }
 
     res.sendStatus(200);
     res.status(200).json({ status: "REQUISIÇAO RECEBIDA" });
-    addItem()
   } else if (req.method === "GET") {
     res.status(200).json({ status: "GET RECEBIDO" });
   } else {
