b76825517db7213f38a64387bed2851075b6cd94
leoleoyuuki <rm550373@fiap.com.br>
2024-08-26 12:17:01 -0300

    feat: Add Firebase SDK and initialize Firestore
    This commit adds the Firebase SDK to the project and initializes Firestore. The necessary dependencies for Firebase have been added to the package.json file. The Firebase configuration has been set up in the firebasesdk.js file, including the API key, project ID, and other necessary details.
    
    Closes #123

---
+++ src/service/firebasesdk.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore,collection, addDoc, getDocs, updateDoc, doc, deleteDoc   } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAOSzMtAz3os1wCYKfd1Fhor1feJGTFcEY",
+  authDomain: "saas-mercadopago.firebaseapp.com",
+  projectId: "saas-mercadopago",
+  storageBucket: "saas-mercadopago.appspot.com",
+  messagingSenderId: "196810760883",
+  appId: "1:196810760883:web:6d1f06a6f9512febe906c6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export {db, collection, addDoc, getDocs, updateDoc, doc, deleteDoc}
