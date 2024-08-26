af7caef2fc79f9aaf5c19a2c97214d54b22838e3
leoleoyuuki <rm550373@fiap.com.br>
2024-08-26 15:05:27 -0300

    environment variables

--- src/service/firebasesdk.js
+++ src/service/firebasesdk.js
@@ -5,14 +5,23 @@ import { getFirestore,collection, addDoc, getDocs, updateDoc, doc, deleteDoc   }
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyAOSzMtAz3os1wCYKfd1Fhor1feJGTFcEY",
-    authDomain: "saas-mercadopago.firebaseapp.com",
-    projectId: "saas-mercadopago",
-    storageBucket: "saas-mercadopago.appspot.com",
-    messagingSenderId: "196810760883",
-    appId: "1:196810760883:web:6d1f06a6f9512febe906c6"
+// const firebaseConfig = {
+//     apiKey: "AIzaSyAOSzMtAz3os1wCYKfd1Fhor1feJGTFcEY",
+//     authDomain: "saas-mercadopago.firebaseapp.com",
+//     projectId: "saas-mercadopago",
+//     storageBucket: "saas-mercadopago.appspot.com",
+//     messagingSenderId: "196810760883",
+//     appId: "1:196810760883:web:6d1f06a6f9512febe906c6"
+//   };
+  const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   };
+  
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
