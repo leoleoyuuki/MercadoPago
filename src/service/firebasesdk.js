13bf0a005b447d152c363574e849c92077cbd082
leoleoyuuki <rm550373@fiap.com.br>
2024-08-26 15:13:53 -0300

    feat: Update Firebase SDK version and initialize Firestore
    This commit updates the Firebase SDK version to 10.13.0 and initializes Firestore in the project. The necessary environment variables for Firebase have been added to the `.env` file. The Firebase configuration has been set up in the `firebasesdk.js` file, using the provided environment variables.
    
    Closes #123

--- src/service/firebasesdk.js
+++ src/service/firebasesdk.js
@@ -5,14 +5,6 @@ import { getFirestore,collection, addDoc, getDocs, updateDoc, doc, deleteDoc   }
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// const firebaseConfig = {
-//     apiKey: "AIzaSyAOSzMtAz3os1wCYKfd1Fhor1feJGTFcEY",
-//     authDomain: "saas-mercadopago.firebaseapp.com",
-//     projectId: "saas-mercadopago",
-//     storageBucket: "saas-mercadopago.appspot.com",
-//     messagingSenderId: "196810760883",
-//     appId: "1:196810760883:web:6d1f06a6f9512febe906c6"
-//   };
   const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
