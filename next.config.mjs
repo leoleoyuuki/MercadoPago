30de9e3e119395a58c03b3d7a0e5253cfa967bb7
leoleoyuuki <rm550373@fiap.com.br>
2024-09-03 11:13:38 -0300

    teste

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['via.placeholder.com', 'another-example.com'],
+    },
+};
 
 export default nextConfig;
