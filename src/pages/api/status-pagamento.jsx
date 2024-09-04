93d1a0a4d677e434ef8dd738e07091113c0b2c5d
leoleoyuuki <rm550373@fiap.com.br>
2024-09-04 18:25:37 -0300

    recebe webhook mercado pago, usa o id do webhook para checkar os pagamentos e então achar o uid gambiarrado como first_name e guardar a assinatura no banco de dados

--- src/pages/api/status-pagamento.jsx
+++ src/pages/api/status-pagamento.jsx
@@ -1,11 +1,13 @@
 
+import { headers } from "next/headers";
 import {addDoc,collection,db} from "../../service/firebasesdk"
 import axios from "axios";
 
-const addItem = async () => {
+const addItem = async (uid) => {
   try {
     const docRef = await addDoc(collection(db, "Pessoas"), {
       Nome: "FUNCIONOU",
+      userId: uid,
     });
     console.log("Document written with ID: ", docRef.id);
 
@@ -22,8 +24,25 @@ export default async function handler(req, res) {
     console.log("requisição recebida", req.body);
     const id = req.body.data.id;
 
-    const email = axios.get(`https://api.mercadopago.com/v1/payments/${id}`);
-    console.log(email);
+
+    try {
+      const response = await axios.get(`https://api.mercadopago.com/v1/payments/${id}`, {
+        headers: {
+          'Authorization': 'Bearer TEST-5451710554063836-081815-2e6df1c787b904657c2fcac56e7b3586-486596499'
+        }
+      });
+    
+      const data = response.data;
+    
+      const uid = data.additional_info.payer.first_name;
+      console.log('Uid do usuário usando gambiarra: ', uid);
+      addItem(uid);
+    
+    } catch (error) {
+      console.error('Erro ao fazer a requisição:', error);
+    }
+    
+    
 
     
     res.status(200).json({ status: "REQUISIÇAO RECEBIDA"});
