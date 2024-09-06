94a5e6043910736f212d3003ba5a2c9020c8eaad
leoleoyuuki <rm550373@fiap.com.br>
2024-09-05 22:30:59 -0300

    chore: Refactor platform page to handle user authentication and subscription status

--- src/app/plataforma/page.jsx
+++ src/app/plataforma/page.jsx
@@ -15,14 +15,8 @@ const PlatformPage = () => {
       if (user) {
         setUser(user); // Usuário logado
         setLoading(true); // Inicia o carregamento enquanto verifica a assinatura
-      } else {
-        setUser(null); // Redefine o estado
-        setLoading(false);
-        setAssinaturaPaga(false);
-        router.push('/'); // Redireciona para tela principal se não estiver logado
-      }
-    });
-    const checkAssinatura = async (uid) => {
+
+        const checkAssinatura = async (uid) => {
           try {
             const assinaturasRef = collection(db, "Assinaturas");
             const q = query(assinaturasRef, where("userId", "==", uid));
@@ -42,7 +36,14 @@ const PlatformPage = () => {
         };
 
         // Verifica a assinatura do usuário logado
-    checkAssinatura(user.uid);
+        checkAssinatura(user.uid);
+      } else {
+        setUser(null); // Redefine o estado
+        setLoading(false);
+        setAssinaturaPaga(false);
+        router.push('/'); // Redireciona para tela principal se não estiver logado
+      }
+    });
 
     return () => unsubscribe(); // Cleanup on component unmount
   }, [auth, router]);
